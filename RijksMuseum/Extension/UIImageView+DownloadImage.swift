b2ba39f8a6e18da6c8f43d5e573325ae85891028
dimo.hamdy <dimo.hamdy@gmail.com>
2022-03-21 11:55:22 +0100

    Remove Cache Expiration to cache images

--- RijksMuseum/Extension/UIImageView+DownloadImage.swift
+++ RijksMuseum/Extension/UIImageView+DownloadImage.swift
@@ -12,14 +12,12 @@ extension UIImageView {
 
     func download(from path: String, contentMode mode: UIView.ContentMode = .scaleAspectFill) {
         guard let url = URL(string: path) else { return }
-        self.kf.indicatorType = .activity
-        self.kf.setImage(
+        kf.indicatorType = .activity
+        kf.setImage(
             with: url,
             placeholder: UIImage(named: "place_holder"),
             options: [
                 .transition(.fade(0.1)),
-                .memoryCacheExpiration(.seconds(200)),
-                .diskCacheExpiration(.seconds(200))
             ])
     }
 }
