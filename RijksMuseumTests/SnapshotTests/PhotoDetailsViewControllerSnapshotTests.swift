4c7b16d64b1548b794a209c251e085224bc93a3f
dimohamdy <dimo.hamdy@gmail.com>
2023-07-09 01:09:29 +0200

    Add Snapshot Test

---
+++ RijksMuseumTests/SnapshotTests/PhotoDetailsViewControllerSnapshotTests.swift
@@ -0,0 +1,47 @@
+//
+//  PhotosListViewControllerTests.swift
+//  PhotosTests
+//
+//  Created by Dimo Abdelaziz on 06/10/2022.
+//
+
+import Foundation
+import SnapshotTesting
+@testable import RijksMuseum
+import XCTest
+import Network
+
+
+final class PhotoDetailsViewControllerSnapshotTests: XCTestCase {
+    var photoDetailsViewController: PhotoDetailsViewController!
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        photoDetailsViewController = nil
+    }
+
+    func test_snapshot_show_ArtObject_details_success() {
+        setVC(internetConnectionState: .satisfied)
+        assertSnapshot(matching: photoDetailsViewController, as: .wait(for: 2, on: .image))
+    }
+
+    func test_snapshot_noInternet_show_ArtObject_success() {
+        setVC(internetConnectionState: .unsatisfied)
+        assertSnapshot(matching: photoDetailsViewController, as: .wait(for: 2, on: .image))
+    }
+}
+
+extension PhotoDetailsViewControllerSnapshotTests {
+
+    func setVC(internetConnectionState: NWPath.Status) {
+        let connectToInternet = MockReachability(internetConnectionState: internetConnectionState)
+        let dataProvider: ArtObjectsRepository =  internetConnectionState == .satisfied ? MockDataPhotosRepository() : MockNoDataPhotosRepository()
+        let artObjectData = DataLoader().loadJsonData(file: "ArtObject")!
+        let artObject = try! JSONDecoder().decode(ArtObject.self, from: artObjectData)
+        photoDetailsViewController = PhotoDetailsBuilder.viewController(photo: artObject, artObjectsRepository: dataProvider, reachable: connectToInternet)
+        let view = photoDetailsViewController.view
+    }
+}
---
+++ RijksMuseumTests/SnapshotTests/PhotosListViewControllerSnapshotTests.swift
@@ -0,0 +1,52 @@
+//
+//  PhotosListViewControllerTests.swift
+//  PhotosTests
+//
+//  Created by Dimo Abdelaziz on 06/10/2022.
+//
+
+import Foundation
+import SnapshotTesting
+@testable import RijksMuseum
+import XCTest
+import Network
+
+final class PhotosListViewControllerSnapshotTests: XCTestCase {
+    var photosListViewController: PhotosListViewController!
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        photosListViewController = nil
+    }
+
+    func test_snapshot_get_onlinePhotos_success() {
+        setVC(internetConnectionState: .satisfied)
+        assertSnapshot(matching: photosListViewController, as: .wait(for: 5, on: .image))
+    }
+
+    func test_snapshot_noResult() {
+        let connectToInternet = MockReachability(internetConnectionState: .satisfied)
+        let dataProvider: ArtObjectsRepository =  MockNoDataPhotosRepository()
+        photosListViewController = PhotosListBuilder.viewController(artObjectsRepository: dataProvider, reachable: connectToInternet)
+        let view = photosListViewController.view
+        assertSnapshot(matching: photosListViewController, as: .wait(for: 5, on: .image))
+    }
+
+    func test_snapshot_noInternet_NoOfflinePhotos() {
+        setVC(internetConnectionState: .unsatisfied)
+        assertSnapshot(matching: photosListViewController, as: .wait(for: 5, on: .image))
+    }
+}
+
+extension PhotosListViewControllerSnapshotTests {
+
+    func setVC(internetConnectionState: NWPath.Status) {
+        let connectToInternet = MockReachability(internetConnectionState: internetConnectionState)
+        let dataProvider: ArtObjectsRepository =  internetConnectionState == .satisfied ? MockDataPhotosRepository() : MockNoDataPhotosRepository()
+        photosListViewController = PhotosListBuilder.viewController(artObjectsRepository: dataProvider, reachable: connectToInternet)
+        let view = photosListViewController.view
+    }
+}
