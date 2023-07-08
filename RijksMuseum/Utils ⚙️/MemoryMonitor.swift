f3ff8100208e7ea700325be672b23b3e28390c11
dimohamdy <dimo.hamdy@gmail.com>
2023-07-09 00:27:07 +0200

    Handle memory warning

---
+++ RijksMuseum/Utils ⚙️/MemoryMonitor.swift
@@ -0,0 +1,25 @@
+//
+//  MemoryMonitor.swift
+//  RijksMuseum
+//
+//  Created by Dimo Abdelaziz on 30/05/2023.
+//
+
+import Foundation
+import UIKit
+import Kingfisher
+
+struct MemoryMonitor {
+
+    static let shared = MemoryMonitor()
+
+    private init() {
+
+    }
+
+    func startMemoryMonitorObserver() {
+        NotificationCenter.default.addObserver(forName: UIApplication.didReceiveMemoryWarningNotification, object: nil, queue: nil) { _ in
+            KingfisherManager.shared.cache.clearMemoryCache()
+        }
+    }
+}
