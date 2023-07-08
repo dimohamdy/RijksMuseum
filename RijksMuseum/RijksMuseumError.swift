3cc5abf5935b2f985424aee50f4400c6cb6d33a5
dimohamdy <dimo.hamdy@gmail.com>
2023-07-09 00:36:01 +0200

    Update RijksMuseumError

--- RijksMuseum/RijksMuseumError.swift
+++ RijksMuseum/RijksMuseumError.swift
@@ -7,23 +7,25 @@
 
 import Foundation
 
-enum RijksMuseumError: Error {
+enum RijksMuseumError: Error, Equatable {
     case failedConnection
+    case wrongURL
     case noResults
     case noInternetConnection
     case runtimeError(String)
     case parseError
     case fileNotFound
-    case wrongURL
+    case invalidServerResponse
+    case canNotLoadMore
 
     var localizedDescription: String {
         switch self {
         case .noResults:
-            return Strings.noResult.localized()
+            return Strings.noResult.localized
         case .noInternetConnection:
-            return Strings.noInternetConnectionTitle.localized()
+            return Strings.noInternetConnectionTitle.localized
         default:
-            return Strings.commonGeneralError.localized()
+            return Strings.commonGeneralError.localized
         }
     }
 }
